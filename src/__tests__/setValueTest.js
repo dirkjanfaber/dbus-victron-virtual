73af30c030f5b3301cde0000d972804cb207820b
Chris Oloff <chris@uber5.com>
2024-07-26 20:40:59 +0200

    test us getting called with SetValue

--- src/__tests__/setValueTest.js
+++ src/__tests__/setValueTest.js
@@ -6,11 +6,8 @@ describe('victron-dbus-virtual, setValue tests', () => {
   it('works for the happy case for settings', async () => {
     const declaration = { name: 'foo', properties: { StringProp: 's' } };
     const definition = { StringProp: 'hello' };
-    const emit = jest.fn();
     const bus = {
-      exportInterface: (iface /* , _path, _ifaceDesc */) => {
-        iface.emit = emit;
-      },
+      exportInterface: () => { },
       invoke: function(args, cb) {
         process.nextTick(() => cb(null, args));
       }
@@ -31,8 +28,25 @@ describe('victron-dbus-virtual, setValue tests', () => {
     expect(setValueResult.member).toBe('SetValue');
   });
 
-  // it('works for the happy case when we get called', async () => {
-
-  // });
+  it('works for the happy case when we get called with SetValue', async () => {
+    const declaration = { name: 'foo', properties: { StringProp: 's' } };
+    const definition = { StringProp: 'hello' };
+    const emit = jest.fn();
+    const interfaces = []
+    const bus = {
+      exportInterface: (iface /* , _path, _ifaceDesc */) => {
+        interfaces.push(iface);
+        iface.emit = emit;
+      },
+      invoke: function(args, cb) {
+        process.nextTick(() => cb(null, args));
+      }
+    }
+    addVictronInterfaces(bus, declaration, definition);
+    expect(!!interfaces[1].SetValue).toBe(true);
+    interfaces[1].SetValue([[{ type: 's' }], ['hello']]);
+    expect(emit.mock.calls[0][0]).toBe('ItemsChanged');
+    expect(emit.mock.calls[0][1]).toEqual([['StringProp', [['Value', ['s', 'hello']], ['Text', ['s', 'hello']]]]]);
+  });
 
 });
