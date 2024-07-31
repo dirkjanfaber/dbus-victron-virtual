aa113f2a46490b9f1e0fa3a8307401d75bf79afc
Chris Oloff <chris@uber5.com>
2024-07-31 16:03:49 +0200

    add setValue() tests

--- src/__tests__/setValueTest.js
+++ src/__tests__/setValueTest.js
@@ -3,6 +3,38 @@ const { addVictronInterfaces } = require('../index');
 
 describe('victron-dbus-virtual, setValue tests', () => {
 
+  it('works for the happy case', async () => {
+    const declaration = { name: 'foo', properties: { StringProp: 's' } };
+    const definition = { StringProp: 'hello' };
+    const bus = {
+      exportInterface: () => { },
+      invoke: function(args, cb) {
+        process.nextTick(() => cb(null, args));
+      }
+    }
+    const { setValue } = addVictronInterfaces(bus, declaration, definition);
+
+    const result = await setValue({
+      path: '/StringProp',
+      value: 'fourty-two',
+      destination: 'foo',
+      interface_: 'foo'
+    });
+    expect(result.member).toBe('SetValue');
+    console.log('result...', result);
+    expect(result.body).toStrictEqual([['s', 'fourty-two']]);
+    expect(result.path).toBe('/StringProp');
+    expect(result.interface).toBe('foo');
+    expect(result.destination).toBe('foo');
+
+    // NOTE: calling setValue() does *not* change the definition. If you want to update the definition,
+    // re-assign it: "definition.StringProp = 'fourty-two';"
+    // ... if you want to notify other processes of the change, you can call emitItemsChanged().
+    // The purpose of setValue() is to change the value in the dbus object, not the definition. This is useful
+    // for settings.
+    expect(definition.StringProp).toBe('hello');
+  });
+
   it('works for the happy case for settings', async () => {
     const declaration = { name: 'foo', properties: { StringProp: 's' } };
     const definition = { StringProp: 'hello' };
@@ -47,6 +79,30 @@ describe('victron-dbus-virtual, setValue tests', () => {
     interfaces[1].SetValue([[{ type: 's' }], ['hello']]);
     expect(emit.mock.calls[0][0]).toBe('ItemsChanged');
     expect(emit.mock.calls[0][1]).toEqual([['StringProp', [['Value', ['s', 'hello']], ['Text', ['s', 'hello']]]]]);
+  });
+
+  it('fails if the underlying invoke() fails', async () => {
+    const declaration = { name: 'foo', properties: { StringProp: 's' } };
+    const definition = { StringProp: 'hello' };
+    const bus = {
+      exportInterface: () => { },
+      invoke: function(_args, cb) {
+        process.nextTick(() => cb(new Error('testing ... invoke failed')));
+      }
+    }
+    const { setValue } = addVictronInterfaces(bus, declaration, definition);
+
+    try {
+      await setValue({
+        path: '/StringProp',
+        value: 'fourty-two',
+        destination: 'foo',
+        interface_: 'foo',
+      });
+      expect(false, 'should have thrown');
+    } catch (e) {
+      expect(e.message).toBe('testing ... invoke failed');
+    }
   });
 
 });
